d1624b2211bc407865d37870012a03328afe5b5c
alifcuy100 <ahmad.alif523@gmail.com>
2020-06-09 15:50:05 +0700

    mapsReady

---
+++ HokepApp/app/src/main/java/com/example/hokepapp/permission_access_activity.java
@@ -0,0 +1,14 @@
+package com.example.hokepapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class permission_access_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_permission_access_activity);
+    }
+}
