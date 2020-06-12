9b03adc18f9ec7519983cd2889c297150e44c3e3
alifcuy100 <ahmad.alif523@gmail.com>
2020-06-12 14:36:58 +0700

    tambah info akun

---
+++ HokepApp/app/src/main/java/com/example/hokepapp/infoAkun.java
@@ -0,0 +1,13 @@
+package com.example.hokepapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class infoAkun extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_akun);
+    }
+}
